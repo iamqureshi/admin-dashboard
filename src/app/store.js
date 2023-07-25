596ba4985d80cce15b20b316b3715c2b22d25f47
samir shaikh <samirshaikh.co.in@gmail.com>
2023-07-25 19:52:19 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchFunctionalitySlice from "./features/searcFunctionality";
 
-export default configureStore({
+export default configureStore({ 
   reducer: {  
     searchReducer: searchFunctionalitySlice,
   },

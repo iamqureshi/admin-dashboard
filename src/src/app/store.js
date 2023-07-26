033aa11e5f741de34d8713fb4080312ca3c16157
samir shaikh <samirshaikh.co.in@gmail.com>
2023-07-26 10:27:34 +0530

    Delete src directory

--- src/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import searchFunctionalitySlice from "./features/searcFunctionality";
-
-export default configureStore({
-  reducer: {
-    searchReducer: searchFunctionalitySlice,
-  },
-});
